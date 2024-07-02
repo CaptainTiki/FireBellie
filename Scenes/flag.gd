c445c0d622863ceff2fc65fd459dca945876a2e5
CptTiki <CptTiki@gmail.com>
2024-07-01 20:28:54 -0400

    Pixel Assets
    work on lunar surface

--- Scenes/flag.gd
+++ Scenes/flag.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+signal player_wins
+
 func _ready():
 	$Area2D.connect("area_entered", on_area_entered)
 
 
 func on_area_entered(_area2d):
-	print("win!")
+	emit_signal("player_wins")
 	
